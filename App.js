f60456e6bc1040c2f3dd0554c046b1fbd8f8599e
CalvinQuashie <quashiecalvin13@gmail.com>
2024-06-28 17:20:24 +0000

    I changed the background color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFB732',
+    backgroundColor: '#FFA500',
     alignItems: 'center',
     justifyContent: 'center',
   },
