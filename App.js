6de1ce4ffba9891b6f8c98ffba3dff94a9b9fc64
CalvinQuashie <quashiecalvin13@gmail.com>
2024-06-28 17:28:13 +0000

    I changed the font size and formatting of the name

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Calvin</Text>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Calvin Quashie</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFA500',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
   },
 });
